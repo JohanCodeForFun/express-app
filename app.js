a87b52f3bdcef3279218e71e3efcdaad6b38fe1d
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-01-26 23:02:13 +0100

    add middleware functions for time and logging

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const authorRouter = require('./routes/authorRouter')
 const bookRouter = require('./routes/bookRouter')
 const indexRouter = require('./routes/indexRouter')
 
+const requestTime = function(req, res, next) {
+  req.requestTime = Date.now()
+  next()
+}
+
+app.use(requestTime)
+app.use(myMiddleware)
+
 app.use('/authors', authorRouter)
 app.use('/books', bookRouter)
 app.use('/', indexRouter)
@@ -14,3 +22,11 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server listening on:", PORT)
 })
+
+function myMiddleware(req, res, next) {
+  console.log("middleware function called")
+
+  req.customProperty = "Hello from myMiddleware"
+
+  next();
+}
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,10 +2,15 @@ const { Router } = require('express');
 
 const indexRouter = Router();
 
-indexRouter.get('/', (req, res) => res.send('All indexs'));
-indexRouter.get('/:indexId', (req, res) => {
-  const { indexId } = req.params;
-  res.send(`index ID: ${indexId}`)
+indexRouter.get('/', (req, res) => {
+  let responseText = 'Hello World!<br>'
+  // res.send('Hello world!')
+  responseText += `<small>Requested at: ${req.requestTime}</small>`
+  
+  res.send(responseText)
+
+  console.log("middleware:", req.customProperty)
+  console.log("request time:", req.requestTime)
 });
 
 module.exports = indexRouter;
