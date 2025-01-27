289c1d0bf10d4698b3e958cd391774da59855abf
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-01-27 21:27:10 +0100

    add CustomNotFoundError for improved error handling in author retrieval

--- controllers/authorControllers.js
+++ controllers/authorControllers.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const CustomNotFoundError = require('../errors/CustomNotFoundError');
 const db = require("../db");
 
 const getAuthorById = asyncHandler(async (req, res) => {
@@ -7,8 +8,7 @@ const getAuthorById = asyncHandler(async (req, res) => {
   const author = await db.getAuthorById(Number(authorId));
 
   if (!author) {
-    res.status(404).send("Author not found");
-    return;
+    throw new CustomNotFoundError("Author not found");
   }
 
   res.send(`Author Name: ${author.name}`);
